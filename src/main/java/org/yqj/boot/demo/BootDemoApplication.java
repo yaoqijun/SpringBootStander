1b91c201b8ac16b9e0380943709fe1b9988d1a9c
yaoqijun <yaoqijunmail@foxmail.com>
2016-06-11 15:52:55 +0800

    add all

--- src/main/java/org/yqj/boot/demo/BootDemoApplication.java
+++ src/main/java/org/yqj/boot/demo/BootDemoApplication.java
@@ -2,6 +2,11 @@ package org.yqj.boot.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by yaoqijun.
@@ -10,6 +15,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * Descirbe:
  */
 @SpringBootApplication
+@Controller
 public class BootDemoApplication {
     public static void main(String[] args) {
 //        SpringApplication.run(BootDemoApplication.class, args);
@@ -17,5 +23,11 @@ public class BootDemoApplication {
                 .banner(new DemoBanner())
                 .sources(BootDemoApplication.class)
                 .run(args);
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String indexPage(){
+        return "this is test index paging info";
     }
 }
